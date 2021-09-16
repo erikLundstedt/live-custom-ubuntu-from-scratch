41d6b73b21544f15e49fae163517d4f2e1a67e95
erik.lundstedt <erik@lundstedt.it>
2021-09-16 12:29:16 +0200

    removed prompts from pacstall installation calls-fix to nor try to install from a local script
    Signed-off-by: erik.lundstedt <erik@lundstedt.it>

--- scripts/config.sh
+++ scripts/config.sh
@@ -43,12 +43,12 @@ function customize_image() {
 	curl -fsSL https://git.io/Jue3Z | bash
 	apt-get install git yadm
 
- #pacstall --disable-prompts -Il #example line
-	pacstall --disable-prompts -Il git
-	pacstall --disable-prompts -Il pacget-git
-	pacstall --disable-prompts -Il neovim-git
-    pacstall --disable-prompts -Il dmenu-distrotube
-	pacstall --disable-prompts -Il st-distrotube
+ #pacstall --disable-prompts -I #example line
+	pacstall --disable-prompts -I git
+	pacstall --disable-prompts -I pacget-git
+	pacstall --disable-prompts -I neovim-git
+    pacstall --disable-prompts -I dmenu-distrotube
+	pacstall --disable-prompts -I st-distrotube
 
 	#    pacstall -I emacs-git
 apt-get install -y \
@@ -87,7 +87,7 @@ apt-get install -y \
 	make install
 	systemctl enable ly.service
 
-	pacstall --disable-prompts -Il awesome-git
+	pacstall --disable-prompts -I awesome-git
 
 
 
